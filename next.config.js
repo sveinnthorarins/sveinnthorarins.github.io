40d2d7ccd3692af122156b287cdbd110a0ba6edd
sveinnthorarins <sveinnthz@gmail.com>
2021-08-30 00:13:03 +0000

    added next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    disableStaticImages: true,
+  },
+};
+
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig,
+);
