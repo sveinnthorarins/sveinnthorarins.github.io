2d9cecef6526aca7157967057f1130ee56e34f92
sveinnthorarins <sveinnthz@gmail.com>
2021-08-30 00:30:15 +0000

    added image optimization for jpeg

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
   reactStrictMode: true,
-  images: {
-    disableStaticImages: true,
-  },
+  handleImages: ['jpeg'],
 });
