2a4ff18c90b2c67c5483057e55f77224f998c397
sveinnthorarins <sveinnthz@gmail.com>
2021-08-31 02:07:48 +0000

    added usage of codinglanguagetag component

--- src/components/showcase/card/index.tsx
+++ src/components/showcase/card/index.tsx
@@ -1,17 +1,20 @@
 import styles from './card.module.scss';
 import Link from 'next/link';
+import CodingLanguageTag from '@/components/codinglanguagetag';
 
 type ShowcaseCardProps = {
   title: string,
   href?: string,
   imageSrc: any,
   imageAlt: string,
+  codeLangOrTech: string,
 };
 
 export default function ShowcaseCard(props: ShowcaseCardProps) {
   if (props.href === undefined) return (
     <div className={styles.showcase__card}>
       <img style={{ width: "100%", height: "100%" }} src={props.imageSrc} alt={props.imageAlt} />
+      <CodingLanguageTag className={styles.card__codetag} codeLangOrTech={props.codeLangOrTech} />
       <div className={styles.card__content}>
         <p className={styles.card__title}>{props.title}</p>
       </div>
@@ -21,6 +24,7 @@ export default function ShowcaseCard(props: ShowcaseCardProps) {
     <Link href={props.href}>
       <a className={styles.showcase__card}>
         <img style={{ width: "100%", height: "100%" }} src={props.imageSrc} alt={props.imageAlt} />
+        <CodingLanguageTag className={styles.card__codetag} codeLangOrTech={props.codeLangOrTech} />
         <div className={styles.card__content}>
           <p className={styles.card__title}>{props.title}</p>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,8 +32,8 @@ export default function Home() {
             Here are some of the best ones that I&apos;ve chosen to showcase:
           </TextSectionText>
           <Showcase>
-            <ShowcaseCard title="Ant Foraging Simulation" href="/project/ant-foraging-simulation" imageSrc={antsimPhoto} imageAlt="Ant simulation project in action" />
-            <ShowcaseCard title="Minecraft Server Sleep Manager" href="/project/minecraft-server-sleep-manager" imageSrc={mssmPhoto} imageAlt="Console application icon" />
+            <ShowcaseCard title="Ant Foraging Simulation" codeLangOrTech="typescript" href="/project/ant-foraging-simulation" imageSrc={antsimPhoto} imageAlt="Ant simulation project in action" />
+            <ShowcaseCard title="Minecraft Server Sleep Manager" codeLangOrTech="c++" href="/project/minecraft-server-sleep-manager" imageSrc={mssmPhoto} imageAlt="Console application icon" />
           </Showcase>
         </TextSection>
         <TextSection>
